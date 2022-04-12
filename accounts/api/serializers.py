622fc5af63e366b875941e16b805b2e6818cb98c
unknown <yzjiangyang123@163.com>
2022-04-12 12:58:59 -0700

    signup, login, logout

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,55 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise ValidationError({
+                'username': 'The username does not exist.'
+            })
+
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=20, min_length=6)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise ValidationError({
+                'username': 'This username has been used.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise  ValidationError({
+                'email': 'This email has been used.'
+            })
+
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return user
