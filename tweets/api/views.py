8690e04f93a84b7ff72e40dbf5393d55396d1dd4
unknown <yzjiangyang123@163.com>
2022-04-15 12:59:46 -0700

    tweet api and unit tests

---
+++ tweets/api/views.py
@@ -0,0 +1,43 @@
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
+from tweets.models import Tweet
+
+
+class TweetViewSet(viewsets.GenericViewSet):
+    serializer_class = TweetSerializerForCreate
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def list(self, request):
+        if 'user_id' not in request.query_params:
+            return Response({
+                'error': 'missing user_id'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user_id = request.query_params['user_id']
+        tweets = Tweet.objects.filter(user_id=user_id).order_by('-created_at')
+        serializer = TweetSerializer(tweets, many=True)
+        return Response({'tweets': serializer.data}, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = TweetSerializerForCreate(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check your input.',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        tweet = serializer.save()
+        return Response(
+            TweetSerializer(tweet).data,
+            status=status.HTTP_201_CREATED
+        )
--- twitter/urls.py
+++ twitter/urls.py
@@ -17,10 +17,12 @@ from accounts.api.views import AccountViewSet, UserViewSet
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename='accounts')
+router.register(r'api/tweets', TweetViewSet, basename='tweets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
